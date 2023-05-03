5543fd6b89b3a6f95a95ead2959472f8371d15ee
Naimur178 <naim.cse17@gmail.com>
2023-05-03 15:20:08 +0600

    react routers added

---
+++ src/components/shared/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css'
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Hello from Header
+                
+            </h2>
+        </div>
+    );
+};
+
+export default Header;
