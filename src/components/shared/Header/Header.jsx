266f6e50c97553204553faf635b58368df3daf3c
Naimur178 <naim.cse17@gmail.com>
2023-05-03 20:32:13 +0600

    Header and footer has been successfully implemented

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -28,13 +28,61 @@ const Header = () => {
                     <a className="btn">Get started</a>
                 </div>
             </div>
+            {/* <div className="navbar bg-green-500 text-neutral-content justify-between lg-ps-3">
+                <a className="btn btn-ghost normal-case text-xl lg:p">daisyUI</a>
+                
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <Link to="/" className='mx-3'>Home</Link>
+                        <Link to="/blog" className='mx-3'>Blog</Link>
+                        <Link to="/login" className='mx-3'>Login</Link>
+                        <Link to="/register" className='mx-3'>Sign up</Link>
+                    </ul>
+                
+                </div>
+
+            </div> */}
+
+            {/* <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <a href="https://flowbite.com/" className="flex items-center">
+
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                    </a>
+                    <div className="flex md:order-2">
+                        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+                        <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                            <span className="sr-only">Open main menu</span>
+                            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" ></path></svg>
+                        </button>
+                    </div>
+                    <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                            <li>
+                                <Link to='/' className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/blog' className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Blog</Link>
+                            </li>
+                            <li>
+                                <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                            </li>
+                            <li>
+                                <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav> */}
+
+
             <div>
                 <div className="carousel w-full">
                     <div id="slide1" className="carousel-item relative w-full">
 
                         <section className="bg-center bg-no-repeat bg-[url('https://img.freepik.com/free-vector/chinese-food-background-illustrated_52683-68274.jpg?w=1380&t=st=1683119667~exp=1683120267~hmac=3f7571feb2c88f9782fb3119bad40f4f9b1cb21615945f7c3a62cfb39718260f')] bg-gray-700 bg-blend-multiply w-full">
                             <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-                                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">We invest in the world’s potential</h1>
+                                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Take the taste of Chinese food</h1>
                                 <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
                                 <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                                     <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
@@ -56,7 +104,7 @@ const Header = () => {
                     <div id="slide2" className="carousel-item relative w-full">
                         <section className="bg-center bg-no-repeat bg-[url('https://img.freepik.com/premium-vector/spicy-food-landscape-banner_622908-61.jpg?w=1380')] bg-gray-700 bg-blend-multiply w-full">
                             <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-                                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">We invest in the world’s potential</h1>
+                                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Take the taste of Chinese food</h1>
                                 <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
                                 <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                                     <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
@@ -77,7 +125,7 @@ const Header = () => {
                     <div id="slide3" className="carousel-item relative w-full">
                         <section className="bg-center bg-no-repeat bg-[url('https://img.freepik.com/free-photo/stir-fry-chicken-sweet-peppers-green-onion-top-view-asian-cuisine_2829-17187.jpg?w=1380&t=st=1683119893~exp=1683120493~hmac=cdee053d573bb8b01d65c945fef960456f7858be5d22c52ded8a6b555354fd78')] bg-gray-700 bg-blend-multiply w-full">
                             <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-                                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">We invest in the world’s potential</h1>
+                                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Take the taste of Chinese food</h1>
                                 <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
                                 <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                                     <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
@@ -98,7 +146,7 @@ const Header = () => {
                     <div id="slide4" className="carousel-item relative w-full">
                         <section className="bg-center bg-no-repeat bg-[url('https://img.freepik.com/free-vector/flat-style-food-twitter-header_23-2149076737.jpg?w=1380&t=st=1683120257~exp=1683120857~hmac=4cb8dd6f5af52b2dfaf2d5d9f944c3e4cb0d3b085c8eadbac8557429537f90cb')] bg-gray-700 bg-blend-multiply w-full">
                             <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-                                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">We invest in the world’s potential</h1>
+                                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Take the taste of Chinese food</h1>
                                 <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
                                 <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                                     <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
